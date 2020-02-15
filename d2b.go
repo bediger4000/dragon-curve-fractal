6432b4690e9d2b7e91343e85d6a8449a82afe186
Bruce Ediger <bediger8@gmail.com>
2020-02-15 14:01:33 -0700

    2-color dragon picture

---
+++ d2b.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Printf("Usage: %s generations real imag [offset x [offset y]]\n", filepath.Base(os.Args[0]))
+		return
+	}
+	generations, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	r, err := strconv.ParseFloat(os.Args[2], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	i, err := strconv.ParseFloat(os.Args[3], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var offsetX, offsetY float64
+	if len(os.Args) > 4 {
+		offsetX, err = strconv.ParseFloat(os.Args[4], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if len(os.Args) > 5 {
+		offsetY, err = strconv.ParseFloat(os.Args[5], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	z := complex(r, i)
+	findLimit(generations, z, offsetX, offsetY)
+}
+
+var const1 complex128 = complex(1, 1)
+var const2 complex128 = complex(1, -1)
+
+func f1(z complex128) complex128 {
+	return z * const1 / 2.0
+}
+func f2(z complex128) complex128 {
+	return 1.0 - z*const2/2.0
+}
+
+func findLimit(ply int, x complex128, offsetX, offsetY float64) {
+	if ply == 0 {
+		x1 := f1(x)
+		x2 := f2(x)
+		fmt.Printf("%f\t%f\t1\n", real(x1)+offsetX, imag(x1)+offsetY)
+		fmt.Printf("%f\t%f\t2\n", real(x2)+offsetX, imag(x2)+offsetY)
+		return
+	}
+	x1 := f1(x)
+	x2 := f2(x)
+	findLimit(ply-1, x1, offsetX, offsetY)
+	if x1 != x2 {
+		findLimit(ply-1, x2, offsetX, offsetY)
+	}
+}
--- makefile
+++ makefile
@@ -1,14 +1,28 @@
+all: dragon.png dragon2.png
 
 dragon.png: d3.pts dragon.load
 	gnuplot < dragon.load
 
+dragon2.png: a.pts b.pts dragon2.load
+	gnuplot < dragon2.load
+
 d3.pts: d3
 	./d3 17 | sort | uniq > d3.pts
+
+a.pts b.pts: d2b
+	./d2b 20 0 0  > c.pts
+	./d2b 20 1 0  > d.pts
+	cat c.pts d.pts > x.pts
+	grep '1$$' x.pts > a.pts
+	grep '2$$' x.pts > b.pts
 
 d3: d3.go
 	go build d3.go
 
+d2b: d2b.go
+	go build d2b.go
+
 clean:
 	-go clean
-	-rm -rf d3.pts
+	-rm -rf d3.pts x.pts a.pts b.pts c.pts d.pts
 	-rm -rf *.png
